1bb28459980f7471d86531e35f7ec2ab7d64c4ed
zhaixl <zhaixinlong@gmail.com>
2024-08-20 11:42:36 +0800

    Initial Commit

---
+++ FoodPicker/FoodPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodPickerApp.swift
+//  FoodPicker
+//
+//  Created by zhaixl on 2024/8/20.
+//
+
+import SwiftUI
+
+@main
+struct FoodPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
